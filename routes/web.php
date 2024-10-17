8bed91eedc4bbe56cddf708de9d9fb4cdfff7e9e
MogahidGaffar <mogahidgaffar@gmail.com>
2024-10-17 17:37:13 +0300

    Delete unused files

--- routes/web.php
+++ routes/web.php
@@ -22,56 +22,44 @@ use App\Http\Controllers\ExportController;
 |
 */
 
-// Route::get('/', function () {
-//     return Inertia::render('Dashboard', [
-//         'canLogin' => Route::has('login'),
-//         'canRegister' => Route::has('register'),
-//         'laravelVersion' => Application::VERSION,
-//         'phpVersion' => PHP_VERSION,
-//     ]);
-// });
 
-Route::get('/',[DashboardController::class,'index'])->middleware(['auth', 'verified'])->name('dashboard');
-Route::get('/dashboard',[DashboardController::class,'index'])->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
 
 
-    
-Route::resource('users',UsersController::class);
-Route::post('users/{user}',[UsersController::class,'update'])->name('users.update');
-Route::post('users/{user}/activate',[UsersController::class,'activate'])->name('activate');
+  Route::resource('users', UsersController::class);
+  Route::post('users/{user}', [UsersController::class, 'update'])->name('users.update');
+  Route::post('users/{user}/activate', [UsersController::class, 'activate'])->name('activate');
 
-    Route::resource('permissions', App\Http\Controllers\PermissionController::class);
-    Route::get('permissions/{permissionId}/delete', [App\Http\Controllers\PermissionController::class, 'destroy']);
+  Route::resource('permissions', App\Http\Controllers\PermissionController::class);
+  Route::get('permissions/{permissionId}/delete', [App\Http\Controllers\PermissionController::class, 'destroy']);
 
-    Route::resource('roles', App\Http\Controllers\RoleController::class);
-    Route::get('roles/{roleId}/delete', [App\Http\Controllers\RoleController::class, 'destroy']);
-    Route::get('roles/{roleId}/give-permissions', [App\Http\Controllers\RoleController::class, 'addPermissionToRole']);
-    Route::put('roles/{roleId}/give-permissions', [App\Http\Controllers\RoleController::class, 'givePermissionToRole']);
-
-    
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-    Route::get('logs', [LogController::class, 'index'])->name('logs');
-    Route::get('logs/{log}', [LogController::class, 'view'])->name('logs.view');
-    Route::post('logs/undo/{log}', [LogController::class, 'undo'])->name('logs.undo');
-
-    Route::get('lang/change', [LangController::class, 'change'])->name('changeLang');
-    // Route::get('/export-users', [ExportController::class, 'export'])->name('export.users');
-   
-    Route::resource('notification', NotificationController::class)
-  ->middleware('auth')
-  ->only(['index']);
+  Route::resource('roles', App\Http\Controllers\RoleController::class);
+  Route::get('roles/{roleId}/delete', [App\Http\Controllers\RoleController::class, 'destroy']);
+  Route::get('roles/{roleId}/give-permissions', [App\Http\Controllers\RoleController::class, 'addPermissionToRole']);
+  Route::put('roles/{roleId}/give-permissions', [App\Http\Controllers\RoleController::class, 'givePermissionToRole']);
 
 
-  
+  Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+  Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+  Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+  Route::get('logs', [LogController::class, 'index'])->name('logs');
+  Route::get('logs/{log}', [LogController::class, 'view'])->name('logs.view');
+  Route::post('logs/undo/{log}', [LogController::class, 'undo'])->name('logs.undo');
+
+  Route::get('lang/change', [LangController::class, 'change'])->name('changeLang');
+
+  Route::resource('notification', NotificationController::class)
+    ->middleware('auth')
+    ->only(['index']);
+
+
+
 });
 
-// Route::get('/export-users', [ExportController::class, 'export'])
-// ->withoutMiddleware(['web'])->name('export.users');
 Route::get('/export-users', [ExportController::class, 'export'])->name('export.users');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
